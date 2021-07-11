efc1fb7dc7a747fbc31804a34ee115e02038dfc9
erick.tavares <erick.tavares@hotmail.com>
2021-07-10 21:41:29 -0300

    Ajustes gerais no projeto

---
+++ src/main/java/br/com/pdi/servicoprestado/rest/ServicoPrestadoRest.java
@@ -0,0 +1,54 @@
+package br.com.pdi.servicoprestado.rest;
+
+import br.com.pdi.cliente.entity.Cliente;
+import br.com.pdi.cliente.service.ClienteService;
+import br.com.pdi.servicoprestado.entity.ServicoPrestado;
+import br.com.pdi.servicoprestado.entity.dto.ServicoPrestadoDTO;
+import br.com.pdi.servicoprestado.service.ServicoPrestadoService;
+import br.com.pdi.util.ConverterBigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicos-prestados")
+@RequiredArgsConstructor
+public class ServicoPrestadoRest {
+
+    private final ClienteService clienteService;
+    private final ServicoPrestadoService servicoPrestadoService;
+    private final ConverterBigDecimal converterBigDecimal;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ServicoPrestado salvar (@RequestBody @Valid ServicoPrestadoDTO dto){
+        LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        int idCliente = dto.getIdCliente();
+
+        Cliente cliente = clienteService.findById(idCliente)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente inexistente"));
+
+        ServicoPrestado servicoPrestado = new ServicoPrestado();
+        servicoPrestado.setDescricao(dto.getDescricao());
+        servicoPrestado.setData(data);
+        servicoPrestado.setCliente(cliente);
+        servicoPrestado.setValor(converterBigDecimal.converter(dto.getPreco()));
+
+        return servicoPrestadoService.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) int mes
+            ){
+        return servicoPrestadoService.buscarPorNomeEMes( "%" + nome + "%" , mes);
+    }
+
+}
---
+++ src/main/java/br/com/pdi/servicoprestado/service/ServicoPrestadoService.java
@@ -0,0 +1,51 @@
+package br.com.pdi.servicoprestado.service;
+
+import br.com.pdi.servicoprestado.entity.ServicoPrestado;
+import br.com.pdi.servicoprestado.repository.ServicoPrestadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicoPrestadoService {
+
+    @Autowired
+    private ServicoPrestadoRepository servicoPrestadoRepository;
+
+    public ServicoPrestado save(ServicoPrestado servicoPrestado){
+      //  servicoPrestado.setData(new LocalDate());
+        return servicoPrestadoRepository.save(servicoPrestado);
+    }
+
+    public Optional<ServicoPrestado> findById (int id){
+        return servicoPrestadoRepository.findById(id);
+    }
+
+    public void deleteById (int id){
+        servicoPrestadoRepository.deleteById(id);
+    }
+
+    public ServicoPrestado update(int id, ServicoPrestado servicoPrestado){
+        Optional<ServicoPrestado> servicoOptional = servicoPrestadoRepository.findById(id);
+        if (servicoOptional.isPresent()){
+            ServicoPrestado servicoAtual = servicoOptional.get();
+//            servicoAtual.setCpf(servicoOptional.getCpf());
+//            servicoAtual.setNome(servicoOptional.getNome());
+
+            servicoPrestadoRepository.save(servicoAtual);
+
+            return servicoAtual;
+        }
+        throw new IllegalArgumentException(String.format("Id %s não encontrado", id));
+    }
+
+    public List<ServicoPrestado> findAll(){
+        return servicoPrestadoRepository.findAll();
+    }
+
+    public List<ServicoPrestado> buscarPorNomeEMes(String nome, int mes) {
+        return servicoPrestadoRepository.findByNomeClienteAndMes(nome, mes);
+    }
+}
